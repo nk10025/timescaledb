76c4bfe1fa5e361d817238c774873c87ffe8b71e
Neeraj Khadka <17919480+nk10025@users.noreply.github.com>
2019-08-15 23:23:45 +1000

    first

---
+++ timescaledbingest.py
@@ -0,0 +1,42 @@
+import random
+from datetime import datetime
+import time
+import json
+import requests
+
+query = """
+mutation {
+  insert_conditions (objects: [
+    {
+      time: "%s",
+      location: "%s",
+      temperature: %.2f,
+      
+      humidity: %.2f
+    }
+  ]) {
+    affected_rows
+  }
+}
+"""
+
+url = 'http://localhost:8080/v1/graphql'
+headers = {
+    'x-hasura-access-key': 'hasuraaccesskey'
+}
+
+location_choices = ['loc1', 'loc2', 'loc3', 'loc4', 'home', 'office']
+
+def mk_query(cur_time, loc, temp, humid):
+    q = query % (cur_time, loc, temp, humid)
+    print(q)
+    r = requests.post(url, data=json.dumps({'query': q}), headers=headers)
+    print(r.text)
+
+while True:
+    temp = random.uniform(18.2, 39.89)
+    humid = random.uniform(40.34, 92.87)
+    loc = random.choice(location_choices)
+    cur_time = datetime.now().isoformat()
+    mk_query(cur_time, loc, temp, humid)
+    time.sleep(0.5)
